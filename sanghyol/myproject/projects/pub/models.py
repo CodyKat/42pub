8dcdc27fe8e7752fc411fda8c447032c7e921e10
Pig30nidaE <Pig30nidaE@gmail.com>
2023-03-16 21:00:59 +0900

    sanghyol: need to fix inventory list print

--- sanghyol/myproject/projects/pub/models.py
+++ sanghyol/myproject/projects/pub/models.py
@@ -8,12 +8,19 @@ class Users(db.Model):
     level = db.Column(db.Integer, nullable=False)
     point = db.Column(db.Integer, nullable=False)
     money = db.Column(db.Integer, nullable=False)
-    itemPossession = db.Column(db.String(100), nullable=True)
 
-
+#Image 추가
 class Market(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    items = db.Column(db.String(100), unique=True, nullable=False)
-    enhance = db.Column(db.Integer, nullable=True)
+    item_name = db.Column(db.String(100), unique=True, nullable=False)
     price = db.Column(db.Integer, nullable=True)
-    
+    grade = db.Column(db.Integer, nullable=True)
+    description = db.Column(db.Text(), nullable=True)
+
+# class-> 유저별로 인벤토리모델을 새로 만들어서, 강화/개수/특히 강화에 따른 같은 아이템도 다르게 판별해야함.
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=True)
+    item_name = db.Column(db.String(100), nullable=True)
+    enhance = db.Column(db.Integer, nullable=True)
+    grade = db.Column(db.Integer, nullable=True)
