758c1ac180d19c471153d9f86ea1ea671f1b5a3d
Pig30nidaE <Pig30nidaE@gmail.com>
2023-03-18 07:26:10 +0900

    sanghyol: bug fix temp ID

--- sanghyol/myproject/projects/pub/api/get_random_username.py
+++ sanghyol/myproject/projects/pub/api/get_random_username.py
@@ -5,19 +5,11 @@ from ..functions.DBcontrol import database
 bp = Blueprint('api_get_random_username', __name__, url_prefix='/api')
 
 from pub.models import Users
-
+from pub import db
 
 @bp.route('/get_random_username', methods=['POST'])
 def get_random_username():
-    print('request!!!!!!')
-    print('request!!!!!!')
-    print('request!!!!!!')
-    print('request!!!!!!')
-    print('request!!!!!!')
-    print('request!!!!!!')
-    print('request!!!!!!')
-    print('request!!!!!!')
     random_username = genRandomUsername.generate_user_id()
     UserDBInfo = Users.Info(user_name=random_username)
     database.upload(UserDBInfo)
-    return jsonify([{'username': f'{random_username}'}])
+    return jsonify({'user_name': f'{random_username}'})
--- sanghyol/myproject/projects/pub/static/make_temp_id.js
+++ sanghyol/myproject/projects/pub/static/make_temp_id.js
@@ -1,37 +1,32 @@
-templogin.addEventListener('click', (event) => {
-    event.preventDefault(); // 기본 폼 제출 동작을 막습니다.
-	fetchUsername();
+const templogin = document.getElementById('templogin');
 
-	async function fetchUsername() {
-		try {
-		  const response = await fetch('http://127.0.0.1:5000/api/get_random_username', {
-			method: 'POST',
-			headers: {
-			  'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ name: 'John' })
-		  });
-
-		  if (!response.ok) {
-			throw new Error('Network response was not ok');
-		  }
-
-		  const data = await response.json();
-		  console.log(data.username); // 'Hello, John!'
-		} catch (error) {
-		  console.error('Error:', error);
-		}
-	  }
-
-const tempID = 'tempID'; // API를 통해 임시 아이디를 발급받아야 합니다.
-
-document.getElementById('makeTempID').addEventListener('click', function() {
-	event.preventDefault();
+templogin.addEventListener('click', async (event) => {
+	event.preventDefault(); // 기본 폼 제출 동작을 막습니다.
+	const tempID = await fetchUsername();
 	document.getElementById('popupText').textContent = "Your temp ID is : " + tempID;
 	document.getElementById('popup').style.display = 'block';
 	document.getElementById('overlay').style.display = 'block';
-
 });
+
+async function fetchUsername() {
+	try {
+		const response = await fetch('http://127.0.0.1:5000/api/get_random_username', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ 'user_name': 'John' })
+		});
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		const data = await response.json();
+		console.log(data.user_name);
+		return data.user_name;
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
 
 document.getElementById('closePopup').addEventListener('click', function() {
 	document.getElementById('popup').style.display = 'none';
