9ef1e170c0364148f9ad2efab75faba2f82e340a
jaemjeon <piepie1346@gmail.com>
2023-03-18 04:07:45 +0900

    jaejeon : edit make_temp_id.js in sanghtol/mypro.... send API get_random_username and print console_log

--- sanghyol/myproject/projects/pub/static/make_temp_id.js
+++ sanghyol/myproject/projects/pub/static/make_temp_id.js
@@ -1,9 +1,20 @@
+const newUserId = fetch('/api/get_random_username', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ name: 'John' })
+})
+.then(response => response.json())
+.then(data => console.log(data.message)) // 'Hello, John!'
+.catch(error => console.error(error));
 
 // API로 임시 아이디를 발급받아야 합니다.
 
 const tempID = 'tempID'; // API를 통해 임시 아이디를 발급받아야 합니다.
 
 document.getElementById('makeTempID').addEventListener('click', function() {
+	event.preventDefault();
 	document.getElementById('popupText').textContent = "Your temp ID is : " + tempID;
 	document.getElementById('popup').style.display = 'block';
 	document.getElementById('overlay').style.display = 'block';
