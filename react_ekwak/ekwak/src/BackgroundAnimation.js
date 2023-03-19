841e61cd01e93b838c1967ca14db6b2d1e2cf04f
jaemjeon <piepie1346@gmail.com>
2023-03-20 02:28:55 +0900

    jaemjeon : merge main_page to react BackgroundAnimaion.js file

--- react_ekwak/ekwak/src/BackgroundAnimation.js
+++ react_ekwak/ekwak/src/BackgroundAnimation.js
@@ -10,70 +10,70 @@ function BackgroundAnimation() {
 
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
-    
+
     const app = new Application({
       width: 500,
       height: 500,
       antialias: true
     });
-    
+
     app.renderer.backgroundColor = 0x202020;
-    
-    
+
+
     app.renderer.resize(screenWidth,screenHeight);
-    
+
     app.renderer.view.style.position = 'absolute';
-    
+
     document.body.appendChild(app.view);
     // 화면 init 하는 공간
     const loginform = document.getElementById('login-form');
-    
+
     // 어떤 이미지가 랜덤으로 돌아다니는 코드
-    
-    
+
+
     const aliens = [];
-    
+
     const totalDudes = 20;
-    
+
     for (let i = 0; i < totalDudes; i++) {
         // create a new Sprite that uses the image name that we just generated as its source
         const dude = PIXI.Sprite.from('/img/건-항아리.png');
-    
+
         // set the anchor point so the texture is centered on the sprite
         dude.anchor.set(0.5);
-    
+
         // set a random scale for the dude - no point them all being the same size!
         dude.scale.set(0.8 + Math.random() * 0.3);
-    
+
         // finally lets set the dude to be at a random position..
         dude.x = Math.random() * app.screen.width;
         dude.y = Math.random() * app.screen.height;
-    
+
         dude.tint = Math.random() * 0xFFFFFF;
-    
+
         // create some extra properties that will control movement :
         // create a random direction in radians. This is a number between 0 and PI*2 which is the equivalent of 0 - 360 degrees
         dude.direction = Math.random() * Math.PI * 2;
-    
+
         // this number will be used to modify the direction of the dude over time
         dude.turningSpeed = Math.random() - 0.8;
-    
+
         // create a random speed for the dude between 2 - 4
         dude.speed = 2 + Math.random() * 2;
-    
+
         // finally we push the dude into the aliens array so it it can be easily accessed later
         aliens.push(dude);
-    
+
         app.stage.addChild(dude);
     }
-    
+
     // create a bounding box for the little dudes
     const dudeBoundsPadding = 100;
     const dudeBounds = new PIXI.Rectangle(-dudeBoundsPadding,
         -dudeBoundsPadding,
         app.screen.width + dudeBoundsPadding * 2,
         app.screen.height + dudeBoundsPadding * 2);
-    
+
     app.ticker.add(() => {
         // iterate through the dudes and update their position
         for (let i = 0; i < aliens.length; i++) {
@@ -82,14 +82,14 @@ function BackgroundAnimation() {
             dude.x += Math.sin(dude.direction) * dude.speed;
             dude.y += Math.cos(dude.direction) * dude.speed;
             dude.rotation = -dude.direction - Math.PI / 2;
-    
+
             // wrap the dudes by testing their bounds...
             if (dude.x < dudeBounds.x) {
                 dude.x += dudeBounds.width;
             } else if (dude.x > dudeBounds.x + dudeBounds.width) {
                 dude.x -= dudeBounds.width;
             }
-    
+
             if (dude.y < dudeBounds.y) {
                 dude.y += dudeBounds.height;
             } else if (dude.y > dudeBounds.y + dudeBounds.height) {
@@ -97,26 +97,26 @@ function BackgroundAnimation() {
             }
         }
     });
-    
+
     //////////////////////////////////////////////
-    
+
     // 가운데 원판이 회전하는 코드
     // 스프라이트를 생성하고 스테이지에 추가합니다.
     const rouletteX = app.view.width / 2;
     const rouletteY = app.view.height * 0.6;
-    
+
     const roulette = PIXI.Sprite.from('/img/룰렛_배경X.png');
     roulette.anchor.set(0.5);
     roulette.x = rouletteX;
     roulette.y = rouletteY;
     app.stage.addChild(roulette);
     ////////////////////////////////
-    
+
     // 애니메이션 루프를 추가하여 컨테이너를 지속적으로 회전시킵니다.
     app.ticker.add((delta) => {
         roulette.rotation += 0.002 * delta; // 회전 속도를 조절할 수 있습니다.
     });
-    
+
     ///////////////////////////////////////////////
     const textStyle = new PIXI.TextStyle({
         fontFamily: 'Arial',
@@ -126,7 +126,7 @@ function BackgroundAnimation() {
         stroke: '#4a1850',
         strokeThickness: 5,
     });
-    
+
     // 텍스트 객체 배열을 생성합니다.
     const mainMeueTexts = [
         new PIXI.Text('나의 정보', textStyle),
@@ -135,16 +135,16 @@ function BackgroundAnimation() {
         new PIXI.Text('42intra', textStyle),
         new PIXI.Text('coalition', textStyle),
     ];
-    
-    
+
+
     const urls = [
         'prev_index.html',
         'shop_page.html',
         'https://profile.intra.42.fr/',
         'https://profile.intra.42.fr/blocs/27/coalitions/85'
     ];
-    
-    
+
+
     for (let i = 0; i < mainMeueTexts.length; i++) {
         mainMeueTexts[i].cursor = 'hover';
         mainMeueTexts[i].interactive = true; // 오타 수정
@@ -158,24 +158,24 @@ function BackgroundAnimation() {
             .on('pointerover', function() {onButtonOver.call(this); })
             .on('pointerout', function() {onButtonOut.call(this); });
     }
-    
+
     // 컨테이너를 생성하고 스테이지에 추가합니다.
     const container = new PIXI.Container();
     app.stage.addChild(container);
-    
+
     // 회전 중심점을 설정합니다.
     container.x = rouletteX;
     container.y = rouletteY;
-    
-    
-    
+
+
+
     function onButtonDown() {
         this.isdown = true;
         this.style = this.style.clone();
         this.style.stroke = '#ffffff';
         this.alpha = 1;
     }
-    
+
     function onButtonUp() {
         this.isdown = false;
         this.style = this.style.clone();
@@ -185,7 +185,7 @@ function BackgroundAnimation() {
             this.style.stroke = '#111111';
         }
     }
-    
+
     function onButtonOver() {
         this.isOver = true;
         if (this.isdown) {
@@ -194,7 +194,7 @@ function BackgroundAnimation() {
         this.style = this.style.clone();
         this.style.stroke = '#777777';
     }
-    
+
     function onButtonOut() {
         this.isOver = false;
         if (this.isdown) {
@@ -205,7 +205,7 @@ function BackgroundAnimation() {
     }
     ///////////////////////////
     // 원판 메뉴들이 돌아가는 코드
-    
+
     app.ticker.add((delta) => {
         container.rotation += 0.002 * delta; // 회전 속도를 조절할 수 있습니다.
         for (let i = 0; i < mainMeueTexts.length; i++)
@@ -214,13 +214,13 @@ function BackgroundAnimation() {
     });
     ///////////////////////////
     // 로그인 직후 보여줘야할 데이터의 투명도를 없애는 코드
-    
+
     let fadeInDuration = 80; // 1000ms = 1초
     let elapsed = 0;
-    
+
     app.ticker.add((delta) => {
         elapsed += delta;
-    
+
         // 페이드 인 애니메이션 진행
         if (elapsed < fadeInDuration) {
             container.alpha = elapsed / fadeInDuration;
@@ -229,11 +229,11 @@ function BackgroundAnimation() {
             app.ticker.remove();
         }
     });
-    
+
     ///////////////////////////
-    
-    
-    
+
+
+
     // 텍스트 객체를 컨테이너에 추가하고 배치합니다.
     mainMeueTexts.forEach((text, index) => {
         text.anchor.set(0.5);
@@ -241,18 +241,18 @@ function BackgroundAnimation() {
         text.y = Math.sin(index * 2 * Math.PI / mainMeueTexts.length) * roulette.y / 2;
         container.addChild(text);
     });
-    
+
     // 마우스가 텍스트 객체 위에 있을 때 텍스트 객체의 색상을 변경합니다.
     container.on('pointerover', (event) => {
         event.target.tint = 0xff0000;
     });
-    
+
     // 마우스가 텍스트 객체 위에서 벗어났을 때 텍스트 객체의 색상을 원래대로 되돌립니다.
     container.on('pointerout', (event) => {
         event.target.tint = 0xffffff;
     });
-    
-    
+
+
     // mouse trail
     // Get the texture for rope.
     const trailTexture = PIXI.Texture.from('/Users/kwak/42pub/sanghyol/myproject/projects/static/assets/img/노란커서.png');
@@ -263,7 +263,7 @@ function BackgroundAnimation() {
     // ropeSize determines how smooth the trail will be.
     const ropeSize = 100;
     const points = [];
-    
+
     // Create history array.
     for (let i = 0; i < historySize; i++) {
         historyX.push(0);
@@ -273,29 +273,29 @@ function BackgroundAnimation() {
     for (let i = 0; i < ropeSize; i++) {
         points.push(new PIXI.Point(0, 0));
     }
-    
+
     // Create the rope
     const rope = new PIXI.SimpleRope(trailTexture, points);
-    
+
     // Set the blendmode
     rope.blendmode = PIXI.BLEND_MODES.ADD;
-    
+
     app.stage.addChild(rope);
-    
-    
+
+
     // 초기 마우스 위치 설정
     const initialPosition = { x: app.view.width / 2, y: app.view.height / 2 };
     let mouseposition = { ...initialPosition };
-    
+
     app.view.addEventListener('mousemove', (event) => {
         mouseposition.x = event.clientX;
         mouseposition.y = event.clientY;
     });
-    
+
     // Listen for animate update
     app.ticker.add(() => {
         if (!mouseposition) return;
-    
+
         // Update the mouse values to history
         historyX.pop();
         historyX.unshift(mouseposition.x);
@@ -304,30 +304,30 @@ function BackgroundAnimation() {
         // Update the points to correspond with history.
         for (let i = 0; i < ropeSize; i++) {
             const p = points[i];
-    
+
             // Smooth the curve with cubic interpolation to prevent sharp edges.
             const ix = cubicInterpolation(historyX, i / ropeSize * historySize);
             const iy = cubicInterpolation(historyY, i / ropeSize * historySize);
-    
+
             p.x = ix;
             p.y = iy;
         }
     });
-    
-    
+
+
     function clipInput(k, arr) {
         if (k < 0) k = 0;
         if (k > arr.length - 1) k = arr.length - 1;
         return arr[k];
     }
-    
+
     function getTangent(k, factor, array) {
         return factor * (clipInput(k + 1, array) - clipInput(k - 1, array)) / 2;
     }
-    
+
     function cubicInterpolation(array, t, tangentFactor) {
         if (tangentFactor == null) tangentFactor = 1;
-    
+
         const k = Math.floor(t);
         const m = [getTangent(k, tangentFactor, array), getTangent(k + 1, tangentFactor, array)];
         const p = [clipInput(k, array), clipInput(k + 1, array)];
@@ -336,7 +336,7 @@ function BackgroundAnimation() {
         const t3 = t * t2;
         return (2 * t3 - 3 * t2 + 1) * p[0] + (t3 - 2 * t2 + t) * m[0] + (-2 * t3 + 3 * t2) * p[1] + (t3 - t2) * m[1];
     }
-    
+
     const profileScreenX = screenWidth * 0.03;
 	const profileScreenY = screenHeight * 0.05;
 	const profileScreenWidth = screenWidth * 0.25;
@@ -536,12 +536,149 @@ function BackgroundAnimation() {
 	app.stage.addChild(title_profile);
 
 
-    ////mainProfile.js 스트립트를 추가합니다
-    //const mainProfile_script = document.createElement('script');
-    //mainProfile_script.src = '/Users/kwak/42pub/sanghyol/myproject/projects/static/mainProfile.js';
-    //document.body.appendChild(mainProfile_script);
-    
-    
+    const haneBannerScreenWidth =  profileScreenWidth;
+    const haneBannerScreenHeight = screenHeight - profileScreenHeight - 20;
+    const haneBannerScreenX =screenWidth * (1 - 0.03) - profileScreenWidth;
+    const haneBannerScreenY = profileScreenY;
+    // 24hane 배너 들어가는 공간
+    const haneBannerField = new PIXI.Graphics();
+
+    console.log(haneBannerScreenX, haneBannerScreenY, haneBannerScreenWidth, haneBannerScreenHeight);
+
+    haneBannerField.beginFill(0x2233BB)
+    .lineStyle(4, 0xFFA00, 1)
+    .drawRect(haneBannerScreenX, haneBannerScreenY, haneBannerScreenWidth, haneBannerScreenHeight)
+    .endFill();
+
+    haneBannerField.alpha = 0.5;
+
+    // 텍스트를 추가합니트
+    const haneBannerTextStyle = new PIXI.TextStyle({
+        fontFamily: 'Arial',
+        fontSize: 30,
+        fontWeight: 'bold',
+        fill: ['#ffffff', '#00ff99'],
+        stroke: '#000000',
+        strokeThickness: 5,
+    });
+
+    const haneBannerText = new PIXI.Text('3월 출입시간 : 168:40:59', haneBannerTextStyle);
+    haneBannerText.x = haneBannerScreenX + 10;
+    haneBannerText.y = haneBannerScreenY;
+
+    app.stage.addChild(haneBannerField);
+    app.stage.addChild(haneBannerText);
+
+
+
+    //////////////////////////////////////////
+    app.ticker.add((delta) => {
+        elapsed += delta;
+
+        // 페이드 인 애니메이션 진행
+        if (elapsed < fadeInDuration) {
+            haneBannerField.alpha = (elapsed / fadeInDuration) * 0.5;
+        } else {
+            // 애니메이션 완료 후, ticker에서 제거
+            app.ticker.remove();
+        }
+    });
+
+    ///////////////////////////////////////////
+
+    const boardScreenX = haneBannerScreenX;
+    const boardScreenY = screenHeight * 0.15;
+    const boardScreenWidth =  profileScreenWidth;
+    const boardScreenHeight =  profileScreenHeight - haneBannerScreenHeight - 10;
+    // 간단 랭킹 정보 들어가는 공간
+    const board_field = new PIXI.Graphics();
+
+    board_field.beginFill(0x2233BB)
+    .lineStyle(4, 0xFFA00, 1)
+    .drawRect(boardScreenX, boardScreenY, boardScreenWidth, boardScreenHeight)
+    .endFill();
+
+    board_field.alpha = 0.5;
+
+    app.stage.addChild(board_field);
+    //////////////////////////////////////////
+    app.ticker.add((delta) => {
+        elapsed += delta;
+
+        // 페이드 인 애니메이션 진행
+        if (elapsed < fadeInDuration) {
+            board_field.alpha = (elapsed / fadeInDuration) * 0.5;
+        } else {
+            // 애니메이션 완료 후, ticker에서 제거
+            app.ticker.remove();
+        }
+    });
+
+    ///////////////////////////////////////////
+
+
+    const rankBoardScreenWidth = screenWidth - profileScreenWidth * 2 - 110;
+    const rankBoardScreenHeight = screenHeight - boardScreenHeight;
+    const rankBoardScreenX = profileScreenX + profileScreenWidth + 10;
+    const rankBoardScreenY = haneBannerScreenY;
+    // 24hane 배너 들어가는 공간
+    const rankBoardField = new PIXI.Graphics();
+
+    console.log(rankBoardScreenX, rankBoardScreenY, rankBoardScreenWidth, rankBoardScreenHeight);
+
+    rankBoardField.beginFill(0x2233BB)
+    .lineStyle(4, 0xFFA00, 1)
+    .drawRect(rankBoardScreenX, rankBoardScreenY, rankBoardScreenWidth, rankBoardScreenHeight)
+    .endFill();
+
+    rankBoardField.alpha = 0.5;
+
+    // 텍스트를 추가합니트
+    const rankBoardTextStyle = new PIXI.TextStyle({
+        fontFamily: 'Arial',
+        fontSize: 60,
+        fontWeight: 'bold',
+        fill: ['#ffffff', '#00ff99'],
+        stroke: '#ffffff',
+        strokeThickness: 5,
+        // 그림자 생성
+        dropShadow: true,
+        dropShadowColor: '#000000',
+        dropShadowBlur: 4,
+        dropShadowAngle: Math.PI / 6,
+        dropShadowDistance: 6,
+    });
+
+    const rankBoardText = new PIXI.Text('RANK', rankBoardTextStyle);
+    rankBoardText.x = (rankBoardScreenX * 2 + rankBoardScreenWidth) / 2 - rankBoardText.width / 2;
+    rankBoardText.y = rankBoardScreenY - 10;
+
+    app.stage.addChild(rankBoardField);
+    app.stage.addChild(rankBoardText);
+
+
+
+
+
+    //////////////////////////////////////////
+    app.ticker.add((delta) => {
+        elapsed += delta;
+
+        // 페이드 인 애니메이션 진행
+        if (elapsed < fadeInDuration) {
+            rankBoardField.alpha = (elapsed / fadeInDuration) * 0.5;
+        } else {
+            // 애니메이션 완료 후, ticker에서 제거
+            app.ticker.remove();
+            // const fill_board_script = document.createElement('script');
+            // fill_board_script.src = './main_board.js';
+            // document.body.appendChild(fill_board_script);
+        }
+    });
+
+
+
+
   }, []);
 
   return <div id="background-animation"></div>;
